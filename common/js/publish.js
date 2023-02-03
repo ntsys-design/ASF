4d81f9e8014474106cffa9140db8aaed0153ebdf
yu jieun <yje3434@ntsys.co.kr>
2023-02-03 17:26:15 +0900

    #230203_울타리관리_등록페이지 작업했음
    울타리관리_등록페이지 작업
    로그인페이지 로그인폼 수정
    울타리관리 페이지 폼 수정

--- common/js/publish.js
+++ common/js/publish.js
@@ -3,3 +3,14 @@ $('nav ul li a').removeClass('active')
 $(this).addClass('active')
 })
 
+$('.adm_menu ul li a').on('click',function(e){
+  e.preventDefault()
+  $('.adm_menu ul li a').removeClass('on')
+  $(this).addClass('on')
+  })
+
+  $('.pagination ul li a').on('click',function(){
+    
+    $('.pagination ul li a').removeClass('active')
+    $(this).addClass('active')
+    })
