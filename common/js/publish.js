6c20ac140616878439605005f887ba872f2ee421
yu jieun <yje3434@ntsys.co.kr>
2023-02-13 17:54:11 +0900

    #230213_작업

--- common/js/publish.js
+++ common/js/publish.js
@@ -14,3 +14,48 @@ $('.pagination ul li a').on('click', function () {
   $('.pagination ul li a').removeClass('active')
   $(this).addClass('active')
 })
+
+$(window).scroll(function () {
+  let scrTop = $(this).scrollTop()
+  $('.ptr_sch_btn').on('click',function(e){
+    e.preventDefault()
+  $('.ptr_modal_box').fadeIn()
+  $('.ptr_modal_box').css({'top' : scrTop + 'px'})
+  // $('body').css({'overflow' : 'hidden'})
+  })
+  
+  $('.close').on('click',function(){
+  $('.modal_box').fadeOut()
+  // $('body').css({'overflow' : 'scroll'})
+  })
+
+  $('.fcnum_sch_btn').on('click',function(e){
+    e.preventDefault()
+  $('.fcnum_modal_box').fadeIn()
+  $('.fcnum_modal_box').css({'top' : scrTop + 'px'})
+  // $('body').css({'overflow' : 'hidden'})
+  })
+
+  $('.nm_sch_btn').on('click',function(e){
+    e.preventDefault()
+  $('.nm_modal_box').fadeIn()
+  $('.nm_modal_box').css({'top' : scrTop + 'px'})
+  // $('body').css({'overflow' : 'hidden'})
+  })
+  
+  
+
+
+
+})
+
+$('.tab_menu li').on('click',function(e){
+  e.preventDefault()
+  let idx=$(this).index()
+  $('.tab_menu li').removeClass('on')
+  $(this).addClass('on')
+  $('.ptr_log').stop().hide()
+  $('.ptr_log').eq(idx).stop().show()
+
+
+})
