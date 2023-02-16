57cf02e01473f068fe2959657b891cedaaf19372
yu jieun <yje3434@ntsys.co.kr>
2023-02-16 21:53:19 +0900

    #0216_레이블,인풋수정
    유지보수작업관리_등록,목록 작업
    순찰 순서 영역 추가

--- common/js/publish.js
+++ common/js/publish.js
@@ -20,51 +20,83 @@ $('.pagination ul li a').on('click', function () {
 // 모달 
 $(window).scroll(function () {
   let scrTop = $(this).scrollTop()
+  //순찰 계획 및 순찰 담당자 / 순찰자 검색
   $('.ptr_sch_btn').on('click',function(e){
     e.preventDefault()
-  $('.ptr_modal_box').fadeIn()
+    $('.dim').css({'display' : 'block'})
+  $('.ptr_modal_box').show()
   $('.ptr_modal_box').css({'top' : scrTop + 'px'})
-  // $('body').css({'overflow' : 'hidden'})
-  })
-  
-  $('.close').on('click',function(){
-  $('.modal_box').fadeOut()
-  // $('body').css({'overflow' : 'scroll'})
+   $('body').css({'overflow' : 'hidden'})
   })
 
+  //순찰 일지 관리 순찰 계획 명 검색
+  $('.ptr_pl_sch_btn').on('click',function(e){
+    e.preventDefault()
+    $('.dim').css({'display' : 'block'})
+  $('.ptr_pl_modal_box').show()
+  $('.ptr_pl_modal_box').css({'top' : scrTop + 'px'})
+   $('body').css({'overflow' : 'hidden'})
+  })
+
+  //순찰 일지 명 검색
+  $('.ptr_log_sch_btn').on('click',function(e){
+    e.preventDefault()
+    $('.dim').css({'display' : 'block'})
+  $('.ptr_log_modal_box').show()
+  $('.ptr_log_modal_box').css({'top' : scrTop + 'px'})
+   $('body').css({'overflow' : 'hidden'})
+  })
+  
+  // 울타리 관리 번호 검색
   $('.fcnum_sch_btn').on('click',function(e){
     e.preventDefault()
-  $('.fcnum_modal_box').fadeIn()
+    $('.dim').css({'display' : 'block'})
+  $('.fcnum_modal_box').show()
   $('.fcnum_modal_box').css({'top' : scrTop + 'px'})
-  // $('body').css({'overflow' : 'hidden'})
+  $('body').css({'overflow' : 'hidden'})
   })
 
-  $('.nm_sch_btn').on('click',function(e){
+  // 공사 실명부 이름 검색
+  $('.nm_list_sch_btn').on('click',function(e){
     e.preventDefault()
-  $('.nm_modal_box').fadeIn()
-  $('.nm_modal_box').css({'top' : scrTop + 'px'})
-  // $('body').css({'overflow' : 'hidden'})
+    $('.dim').css({'display' : 'block'})
+  $('.nm_list_modal_box').show()
+  $('.nm_list_modal_box').css({'top' : scrTop + 'px'})
+  $('body').css({'overflow' : 'hidden'})
   })
-  
-  
 
+// 상단 클로즈 버튼
+  $('.close').on('click',function(){
+  $('.modal_box').hide()
+  $('.dim').css({'display' : 'none'})
+  $('body').css({'overflow' : 'auto'})
 
+  })
 
+  // 하단 닫기 버튼
+  $('.md_cancel_btn').on('click',function(){
+    $('.modal_box').hide()
+    $('.dim').css({'display' : 'none'})
+    $('body').css({'overflow' : 'auto'})
+    })
 })
+
+
+
 // 순찰일지 관리 탭
 $('.tab_menu li').on('click',function(e){
   e.preventDefault()
   let idx=$(this).index()
   $('.tab_menu li').removeClass('on')
   $(this).addClass('on')
-  $('.ptr_log').stop().hide()
-  $('.ptr_log').eq(idx).stop().show()
+  $('.tab_con').stop().hide()
+  $('.tab_con').eq(idx).stop().show()
 
 
 })
 
 // 임시저장 버튼 클릭 탭 이동
-$('.ch_list_btn').click(function(){
+$('.tab2_btn').click(function(){
   
   let href=$(this).attr('href')
 console.log(href)
@@ -74,7 +106,7 @@ $('.tab_menu li').removeClass('on')
 $('.tab_menu li:nth-child(2)').addClass('on')
 })
 
-$('.phm_btn').click(function(){
+$('.tab3_btn').click(function(){
   
   let href=$(this).attr('href')
 console.log(href)
@@ -84,7 +116,7 @@ $('.tab_menu li').removeClass('on')
 $('.tab_menu li:nth-child(3)').addClass('on')
 })
 
-$('.nm_list_btn').click(function(){
+$('.tab4_btn').click(function(){
   
   let href=$(this).attr('href')
 console.log(href)
